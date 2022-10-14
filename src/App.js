c07329fb62f3fcb60d71253e8ab7b393cebff19d
sourav199 <souravdhar1999@gmail.com>
2022-10-14 12:29:33 -0400

    Loop and Callback fucntion

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [one, Setone] = useState([""]);
   return (
     <div className="App">
       <header className="App-header">
         <h1> Hi this is for Interview Prep </h1>
+        <button
+          onClick={() => {
+            Setone([...one, "Callback"]);
+          }}
+        >
+          Call Back Fucntion
+        </button>
+        {
+          <p>
+            {one.map((item) => (
+              <li>{item}</li>
+            ))}
+          </p>
+        }
       </header>
     </div>
   );
